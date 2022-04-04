49aa505f0da0075ffdc10f1c90a4a726f65331f2
Simon Poudyal <simon98190@gmail.com>
2022-04-04 23:43:29 +0545

    added start date to register

--- api/register/register.service.js
+++ api/register/register.service.js
@@ -3,8 +3,8 @@ const pool = require("../../config/database");
 module.exports = {
   create: (data, callBack) => {
     pool.query(
-      `insert into user(firstName, lastName, email, password, phone, city, countryId) 
-                values(?,?,?,?,?,?,?)`,
+      `insert into user(firstName, lastName, email, password, phone, city, countryId, startDate) 
+                values(?,?,?,?,?,?,?,?)`,
       [
         data.FirstName,
         data.LastName,
@@ -13,6 +13,7 @@ module.exports = {
         data.Phone,
         data.City,
         data.CountryId,
+        data.StartDate,
       ],
       (error, results, fields) => {
         if (error) {
